d2caf82b9173766c84d2b7c5b057945c4ceca7e9
elviratemirhanova@mail.ru <sdk5678sdk>
2020-09-16 19:47:05 +0300

    Double type's problems

---
+++ Problems/Find X/src/Main.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val a = scanner.nextDouble()
+    val b = scanner.nextDouble()
+    val c = scanner.nextDouble()
+    val x = (c - b) / a
+    print(x)
+}
